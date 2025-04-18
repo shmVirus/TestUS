0b0de495e89ee1f6e5bab3cc55c4c94224add4b9
Sabbir <64514438+shmVirus@users.noreply.github.com>
2025-04-18 10:40:11 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        test
+// @namespace   Violentmonkey Scripts
+// @match       test
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 4/18/2025, 10:39:41 AM
+// ==/UserScript==
